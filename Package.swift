f3a54cfdf5df0c23ccd6c1ae98ce6950ff7e8afc
daemooon <daemooon@users.noreply.github.com>
2023-03-10 02:17:05 +0000

    XrayKit Release 0.1.30!!!

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "XrayKit",
-      url: "https://github.com/daemooon/XrayKit/releases/download/0.1.29/XrayKit.xcframework.zip",
-      checksum: "47287ff76cf79931d73c3e5c497695cb9a8909995f95b9725b125626d7c35389"
+      url: "https://github.com/daemooon/XrayKit/releases/download/0.1.30/XrayKit.xcframework.zip",
+      checksum: "aa958b33f24e2d49ff6b959c5fe8f200fbf190535764087de9f653969a807227"
     )
   ]
 )
