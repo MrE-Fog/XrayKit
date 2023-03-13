50b3e926b23f3bf82fc929ca3b99338e7decc29f
daemooon <daemooon@users.noreply.github.com>
2023-03-13 03:55:18 +0000

    XrayKit Release 0.1.33!!!

--- Package.swift
+++ Package.swift
@@ -1,17 +1,17 @@
 // swift-tools-version: 5.7
-
 import PackageDescription
 
 let package = Package(
   name: "XrayKit",
+  platforms: [.iOS(.v16)],
   products: [
     .library(name: "XrayKit", targets: ["XrayKit"])
   ],
   targets: [
     .binaryTarget(
       name: "XrayKit",
-      url: "https://github.com/daemooon/XrayKit/releases/download/0.1.30/XrayKit.xcframework.zip",
-      checksum: "aa958b33f24e2d49ff6b959c5fe8f200fbf190535764087de9f653969a807227"
+      url: "https://github.com/daemooon/XrayKit/releases/download/0.1.33/XrayKit.xcframework.zip",
+      checksum: "bf8103f2435ba5316f791813882726e9b00cb5c4319edbb076e88b6606a2e8e1"
     )
   ]
 )
