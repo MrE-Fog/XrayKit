1a2157f6aeb558005a3f92c957e7216f658ec55a
arror <arror@users.noreply.github.com>
2023-04-11 05:25:45 +0000

    XrayKit Release 0.1.56!!!

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "XrayKit",
-      url: "https://github.com/arror/XrayKit/releases/download/0.1.55/XrayKit.xcframework.zip",
-      checksum: "0e60c289aa61ec261e586e4532981d245df0914382507f677c8455f2ffacae0e"
+      url: "https://github.com/arror/XrayKit/releases/download/0.1.56/XrayKit.xcframework.zip",
+      checksum: "a4178ddc5f623984b1bc2424c33cd1d52fd9a347fc2b8a6721d242f91f459dcf"
     )
   ]
 )
